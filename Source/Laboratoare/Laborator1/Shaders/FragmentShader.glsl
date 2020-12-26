8f80617b9bb1c6580520ea3416a378e5c206f951
unknown <lavinialoghin7@gmail.com>
2020-12-26 21:53:53 +0200

    added lab 1

---
+++ Source/Laboratoare/Laborator1/Shaders/FragmentShader.glsl
@@ -0,0 +1,19 @@
+#version 430
+
+layout(location = 0) in vec2 texture_coord;
+
+uniform sampler2D texture_1;
+
+layout(location = 0) out vec4 out_color;
+
+void main()
+{
+	//TODO apply texture
+	vec4 color = texture2D(texture_1, texture_coord);
+	//TODO discard when alfa component < 0.75
+	if(color.a<0.7f) {
+	 discard;
+	}
+
+	out_color = color;
+}
